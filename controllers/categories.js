fcf8bf175f1d8de14e2ea6ed977830c726e5efb5
Lisa <md.nuralisa@gmail.com>
2023-11-14 12:01:40 +0800

    categories api

---
+++ controllers/categories.js
@@ -0,0 +1,116 @@
+import categories from "../model/categories.model.js";
+
+const store = async (req, res) => {
+    const { name }  = req.body;
+    
+    try {
+        const newCategories = await categories.create({
+            name: name,
+        });
+        res.status(200).json({
+            message: "New categories created",
+            data: { name: newCategories.name },
+        });
+        
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error });
+    }
+    
+};
+
+const deleteCategories = async (req, res) => {
+    const { name } = req.body;
+    
+    try {
+        
+        // query user based on name
+        const existCategories = await categories.findOne({
+            where: {
+                name: name,
+            },
+        });
+        
+        // No name return
+        //   if name not found return 404
+        if (!existCategories) {
+            res.status(404).json({ message: "categories not found" });
+            return;
+        }
+        
+        else // Delete 
+        {await existCategories.destroy({
+            where: {
+                id: categories.id
+            },
+        });};
+        
+        res.status(200).json({ message: "categories deleted" });
+        return;
+        
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error });
+    }
+}
+
+const update = async (req, res) => {
+    const { name, newName } = req.body;
+    try {
+        // query user based on name
+        const existCategories = await categories.findOne({
+            where: {
+                name: name,
+            },
+        });
+        
+        // No name return
+        //   if name not found return 404
+        if (!existCategories) {
+            res.status(404).json({ message: "categories not found" });
+            return;
+        }
+        else { 
+            // Change everyone without a last name to "Doe"
+            await categories.update({ name: newName }, {
+                where: {
+                    id: existCategories.id,
+                },
+            });
+            
+            res.status(200).json({ message: "new name for categories updated" });
+            return;
+            
+        }  
+        
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error });
+    }
+}
+
+const show = async (req, res) => {
+    const { name } = req.body;
+    try {
+        // query user based on name
+        const existCategories = await categories.findOne({
+            where: {
+                name: name,
+            },
+        });
+        
+        // No name return
+        //   if name not found return 404
+        if (!existCategories) {
+            res.status(404).json({ message: "categories not found" });
+            return;
+        }
+        else {            
+            res.status(200).json({ message: "categories found", data: {id: existCategories.id} });
+            return;           
+        }  
+        
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error });
+    }
+}
+const categoriesController = { store, deleteCategories, update, show };
+
+export default categoriesController;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import publicController from "../controllers/public.js";
 import authController from "../controllers/auth.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
+import categoriesController from "../controllers/categories.js";
 
 
 const apiRoutes = Router();
@@ -14,5 +15,10 @@ apiRoutes.post("/login", authController.login);
 
 apiRoutes.get("/public", authController.publicController);
 apiRoutes.get("/protected", isAuthenticated, authController.protectedController);
+
+apiRoutes.post("/store", categoriesController.store);
+apiRoutes.delete("/delete", categoriesController.deleteCategories);
+apiRoutes.put("/update", categoriesController.update);
+apiRoutes.get("/show", categoriesController.show);
 
 export default apiRoutes;

b4e5a33b6ce3b2fddb62103261f9f9e3567295e5
Amirul <amirul.naim@zuscoffee.com>
2023-11-15 10:44:14 +0800

    Fix

--- controllers/categories.js
+++ controllers/categories.js
@@ -127,13 +127,18 @@ const listing = async (req, res) => {
     const userId = req.userId;
     try {
 
-        const category = (await categories.findAll({ include: userId }))[0];
+        const data = (await categories.findAll({ 
+            where: {
+                userId: userId,
+            },
+            attributes: ['id', 'name'],
+        }));
 
-        res.status(200).json({ message: "categories found", data: (category, null, 2)});
-            return;
+        res.status(200).json({ message: "categories found", data: data});
 
-        
+        return;
     } catch (error) {
+        console.log(error);
         res.status(500).json({ message: "Server error", error: error });
     }
 }

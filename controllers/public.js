dcadb8cce82124f83d9009df457f9d286271803e
Lisa <md.nuralisa@gmail.com>
2023-11-20 11:37:49 +0800

    test

--- controllers/public.js
+++ controllers/public.js
@@ -1,5 +1,5 @@
 const get = (req, res) => {
-    res.status(200).json({ message: "This is a GET rest api" });
+    res.status(200).json({ message: "This is a GET rest api from production" });
   };
   
   const post = (req, res) => {
